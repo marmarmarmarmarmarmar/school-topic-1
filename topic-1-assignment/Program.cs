3fd710ac0d7b5b6bdc6a1e476963562021c43576
oranar <poraporo@protonmail.com>
2024-09-13 13:22:45 -1100

    test

--- topic-1-assignment/Program.cs
+++ topic-1-assignment/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Coda | 2024/09/08
+Topic 1 - Printing and Comments - Assignment
+*/
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+            Console.Write("Halo!"); // This line will write a phrase, that phrase being "Halo!". I bet you didn't know that one!!!!!!
+            Thread.Sleep(3000);
+            Console.WriteLine(" My name is Coda, also known as 'marmarmarmarmarmarmar...'"); 
+            Console.WriteLine("");
+            Thread.Sleep(5000);
+            Console.Write("In this program, I am using comments,");
+            Thread.Sleep(3000);
+            Console.WriteLine(" which I can use to put any information I want in the program file!");
+            Thread.Sleep(5000);
+            // Console.Write("this message will never be received");
+            Console.WriteLine("Or I can comment a statement/line so it will not execute, for testing, such as the one above this line which was never executed!");
+            Console.WriteLine("");
+            Thread.Sleep(6000);
+            Console.WriteLine(" cccccc    sssssss");
+            Thread.Sleep(500);
+            Console.WriteLine("cc         ss");
+            Thread.Sleep(500);
+            Console.WriteLine("cc         sssssss");
+            Thread.Sleep(500);
+            Console.WriteLine("cc              ss");
+            Thread.Sleep(500);
+            Console.WriteLine(" cccccc .. sssssss ..");
+            Console.WriteLine("");
+            Thread.Sleep(3000);
+            Console.WriteLine("Those are my initials!!!!!");
+            Thread.Sleep(3000);
+            Console.WriteLine("Anyways, no more program, I am done writing");
+            Thread.Sleep(2000);
+        }
+    }
+}
